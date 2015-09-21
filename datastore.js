1518fe8b2e73c1a9583e10d9e67be9777132ec3b
Steffen <steffen.mader@gdi-service.de>
2015-09-21 12:41:40 +0200

    -first attempt on json-store and PointFeatures -added preliminary icon images for map icons -3rd-party code ist stored locally now (folder ./3rdparty/) -switched to ol-debug.js

---
+++ datastore.js
@@ -0,0 +1,33 @@
+store = [
+  {
+    name: "Schöne Aussicht",
+    kategorie: 'ph',
+    lat: 53.480467, 
+    lon: 11.665453
+  },{
+    name: "Alte Liebe",
+    kategorie: 'ph',
+    lat: 53.414328,
+    lon: 11.450418
+  },{
+    name: "Herbstrose",
+    kategorie: 'bw',
+    lat: 53.377493,
+    lon: 11.591940
+  },{
+    name: "Altrosa",
+    kategorie: 'bw',
+    lat: 53.318713,
+    lon: 11.491227
+  },{
+    name: "Bingohalle",
+    kategorie: 'tp',
+    lat: 53.334366,
+    lon: 11.498341 
+  }
+];
+kategoriemap = {
+    ph: 'Pflegeheim',
+    tp: 'Tagespflege-Einrichtung',
+    bw: 'Betreutes Wohnen'
+}
