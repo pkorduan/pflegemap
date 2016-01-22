5322437a004f2b6c51d8ec21ef10f101e9902c47
Peter Korduan <peter.korduan@gdi-service.de>
2016-01-22 10:33:09 +0100

    Rm config.js

--- javascripts/config.js
+++
@@ -1,8 +0,0 @@
-PflegeMap.config = {
-//  storeUrl: '../wfs2json/json/data.json',
-  storeUrl: 'data.json',
-  indexUrl: '../json2index/index/index.json',
-  reachUrl: '../reach/reach.php',
-  viewProjection: 'EPSG:25833',
-  baseProjection: 'EPSG:4326',
-}
