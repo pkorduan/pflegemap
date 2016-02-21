e518cda42b66f60d9f2d17b799f0123463476046
Peter Korduan <peter.korduan@gdi-service.de>
2016-02-21 11:53:19 +0100

    Development: Add config.ini in osm2poServiceProxy to configure the url of the service.

--- osm2poServiceProxy.php
+++ osm2poServiceProxy.php
@@ -1,6 +1,8 @@
 <?php
+  $config = parse_ini_file('config.ini', true);
 #  header ('Content-type: application/json; charset=utf-8');
-  $service_url = 'http://gdi-service.de:8888/Osm2poService';
+#  $service_url = 'http://gdi-service.de:8888/Osm2poService';
+  $service_url = $config['router']['url'];
   $content = @file_get_contents($service_url . '?' . $_SERVER['QUERY_STRING']);
   if ($content === false) {
     echo "#Fehler: 404
