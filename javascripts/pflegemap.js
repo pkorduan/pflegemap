6cf331b9c059e02cde5efb9956d6f13a725f1975
Peter Korduan <peter.korduan@gdi-service.de>
2017-03-09 10:01:08 +0100

    Fix attributation links.

--- javascripts/pflegemap.js
+++ javascripts/pflegemap.js
@@ -97,16 +97,16 @@ PflegeMap.initMap = function(store) {
 					html: 'Kartenbild &copy; <a href="https://www.orka-mv.de/datenschutz.html" target="_blank">Hansestadt Rostock</a> (CC BY 4.0) | Kartendaten &copy; <a href="http://www.openstreetmap.org/about" target="_blank">OpenStreetMap</a> (ODbL) und LkKfS-MV'
 				}),
 				new ol.Attribution({
-					html: '<br><a href="https://nordost.aok.de/" target="_blank">AOK Nordost - Die Gesundheitskasse</a>'
+					html: '<br><a href="https://nordost.aok.de" target="_blank">AOK Nordost - Die Gesundheitskasse</a>'
 				}),
 				new ol.Attribution({
 					html: '<br><a href="http://www.kvmv.info/patienten/40/index.html" target="_blank">Kassenärztliche Vereinigung Mecklenburg-Vorpommern</a>'
 				}),
 				new ol.Attribution({
-					html: '<br><a href="http://www.lagus.mv-regierung.de/" target="_blank">Landesamt für Gesundheit und Soziales Mecklenburg-Vorpommern</a>'
+					html: '<br><a href="http://www.lagus.mv-regierung.de" target="_blank">Landesamt für Gesundheit und Soziales Mecklenburg-Vorpommern</a>'
 				}),
 				new ol.Attribution({
-					html: '<br>Sonstige Daten <a href="http://www.kreis-lup.de/Landkreis" target="_blank">Landkreis Ludwigslust-Parchim</a>'
+					html: '<br>Sonstige Daten <a href="http://www.kreis-lup.de" target="_blank">Landkreis Ludwigslust-Parchim</a>'
 				}),
 			],
       projection: PflegeMap.viewProjection,
